225dc5c5ddc597c40d1b45178614f8ae6ce069c0
Abdifatah Mohamed <abdi.m.official@gmail.com>
2024-02-18 22:03:37 -0800

    added movies to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Oppenheimer', :rating => 'R',
+    :release_date => '21-Jul-2023'},
+  {:title => 'Tenet', :rating => 'PG-13',
+    :release_date => '03-Sep-2020'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '13-Jul-2010'}
 ]
 
 more_movies.each do |movie|
